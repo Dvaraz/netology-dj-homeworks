f1c314dbfa5bf1008370a35428b5139ac8541ddc
Dvaraz <dvaraz1988@gmail.com>
2022-05-13 19:35:48 +0300

    Homework 2.1 done

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .models import Book
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all().order_by('pub_date')
+    context = {'books': books}
+    return render(request, template, context)
+
+
+def book_view(request, pub_date):
+    """
+    prev_page, next_page  - чтобы получить информацию о предыдущей, следующей страницах и достать pub_date
+    для отображения ссылок в виде даты публикации.
+    """
+    template = 'books/books_list.html'
+    books = Book.objects.all().order_by('pub_date')
+    page_number = 0
+    if not request.GET:
+        for book in books:
+            page_number += 1
+            if str(book.pub_date) == pub_date:
+                break
+    else:
+        page_number = int(request.GET.get('page', 0))
+
+    paginator = Paginator(books, 1)
+    page = paginator.get_page(page_number)
+    prev_page = paginator.get_page(page_number - 1)
+    next_page = paginator.get_page(page_number + 1)
+    context = {'books': page,
+               'prev_page': prev_page,
+               'next_page': next_page
+               }
     return render(request, template, context)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,19 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    filter_ = request.GET.get('sort', 'name')
+    if filter_ == 'min_price':
+        phones = Phone.objects.all().order_by('price')
+    elif filter_ == 'max_price':
+        phones = Phone.objects.all().order_by('-price')
+    else:
+        phones = Phone.objects.all().order_by(filter_)
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
