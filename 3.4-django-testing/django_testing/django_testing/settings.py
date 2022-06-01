30c185a9a7374ac47486e14a345edecd7fe9b6cc
Dvaraz <dvaraz1988@gmail.com>
2022-06-01 23:41:11 +0300

    Homework 3.4 done

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -77,15 +77,20 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DB_NAME = 'netology_django_testing'
+DB_USER = 'postgres'
+DB_PASSWORD = '45187'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -124,3 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+}
+
+MAX_STUDENTS_PER_COURSE = 20
--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from rest_framework import serializers
 
 from students.models import Course
@@ -8,3 +9,9 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    # def validate(self, data):
+    #     students = Course.objects.filter(id=self.instance.id).annotate(num_students=Count('students'))
+    #     if students[0].num_students >= 20 and (self.context["request"].method == 'POST' or len(data['students']) > 0):
+    #         raise serializers.ValidationError('Can be only 20 students per course')
+    #     return data
